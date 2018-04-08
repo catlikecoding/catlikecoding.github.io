2fa0ced3118d14b68f85e3a91fdf6fb7ccb363bb
catlikecoding <36832326+catlikecoding@users.noreply.github.com>
2018-04-08 22:51:56 +0700

    Add files via upload

---
+++ project-title/app-files/data.js
@@ -0,0 +1,119 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pankp1_fusedvar1",
+      "name": "pankp1_fusedvar1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 789,
+      "initialViewParameters": {
+        "yaw": -0.25716646890306727,
+        "pitch": -0.15061647630615305,
+        "fov": 1.343902234462253
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8643172993228632,
+          "pitch": -0.014355080523856145,
+          "rotation": 0,
+          "target": "1-panorama"
+        },
+        {
+          "yaw": 1.378637832354059,
+          "pitch": -0.2367643056299258,
+          "rotation": 0,
+          "target": "1-panorama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4370103813944368,
+          "pitch": -0.23030847942288624,
+          "title": "Аудитория 403",
+          "text": "Такой-то текст, узнайте то-то сё-то"
+        },
+        {
+          "yaw": 2.4553906401423458,
+          "pitch": -0.32867089682173933,
+          "title": "Наши компы и прочее",
+          "text": "Текст текст<div>текст текс</div><div>текст текс</div><div>тест</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama",
+      "name": "panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -2.182962996821761,
+        "pitch": 0.4755449560053471,
+        "fov": 1.343902234462253
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8322299681467715,
+          "pitch": 0.42344026375844557,
+          "rotation": 0,
+          "target": "0-pankp1_fusedvar1"
+        },
+        {
+          "yaw": -2.81448903135694,
+          "pitch": 0.48275999329842456,
+          "rotation": 0,
+          "target": "0-pankp1_fusedvar1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0709579112228553,
+          "pitch": 0.4746645392646638,
+          "title": "Текст ектст етктсттектт",
+          "text": "текткттакта тташытомжтывдлмвлтмолтмтлдытмолдытвмтвоымтвыоатмлоавытмолтыолтмовлаытмоавытмоавтмлтылтмавтмлтлоаы<div><br></div><div>ьсьвылмлывмлт</div><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
