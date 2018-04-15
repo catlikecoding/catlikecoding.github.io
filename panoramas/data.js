ed6689eda23d7fc06668c7c1d053450cae984c5a
catlikecoding <36832326+catlikecoding@users.noreply.github.com>
2018-04-16 03:45:57 +0700

    Add files via upload

---
+++ panoramas/data.js
@@ -0,0 +1,151 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-0",
+      "name": "0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9208743790824929,
+        "pitch": 0.08115837408418436,
+        "fov": 1.343902234462253
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09949004236994696,
+          "pitch": 0.04723282283123709,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6254147591278763,
+          "pitch": 0.07208291826581892,
+          "title": "Холл вуза",
+          "text": "Главный холл РЭУ им. Г.В. Плеханова. Здесь располагается гардероб, банкомат, автомат с кофе, а также стенды от партнеров для зарядки смартфонов."
+        },
+        {
+          "yaw": -2.625453164979694,
+          "pitch": 0.03297694477306834,
+          "title": "Вход во второй корпус вуза",
+          "text": "Холл соединяет два корпуса РЭУ им. Г.В. Плеханова."
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.1279809085706614,
+        "pitch": 0.01995442622419752,
+        "fov": 1.343902234462253
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1245377490555635,
+          "pitch": 0.006181479812060076,
+          "rotation": 0,
+          "target": "0-0"
+        },
+        {
+          "yaw": 2.9282421535412286,
+          "pitch": 0.023883238656321026,
+          "rotation": 0,
+          "target": "2-00"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-00",
+      "name": "00",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20618121830555047,
+        "pitch": 0.0901752581565578,
+        "fov": 1.343902234462253
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2511072715897651,
+          "pitch": 0.0331154105665874,
+          "rotation": 0,
+          "target": "0-0"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5339953530652863,
+          "pitch": -0.35980834275526874,
+          "title": "Физкультурный зал",
+          "text": "Зал для занятия физкультурой. Зачастую студенты ходят в спортивный зал \"Максимум\", арендуемый институтом."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
